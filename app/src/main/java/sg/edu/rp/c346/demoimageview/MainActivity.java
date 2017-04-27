f4876afcd534acbe60691b5c1524af0e76500e38
Herwan Shah <shahherwan@hotmail.com>
2017-04-27 10:33:01 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
@@ -0,0 +1,21 @@
+package sg.edu.rp.c346.demoimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView ivDay2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the ImageView obj and assign to ivDay2
+        ivDay2 = (ImageView) findViewById(R.id.imageView2);
+        // Set the image
+        ivDay2.setImageResource(R.drawable.day2);
+    }
+}
