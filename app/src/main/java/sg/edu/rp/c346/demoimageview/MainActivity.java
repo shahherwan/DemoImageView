2333f2e5e298e3087a410d51393189ec08773038
Herwan Shah <shahherwan@hotmail.com>
2017-04-27 10:35:17 +0800

    added imageview 1

--- app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
@@ -6,7 +6,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView ivDay2;
+    ImageView ivDay2, ivDay1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +14,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get the ImageView obj and assign to ivDay2
+        ivDay1 = (ImageView) findViewById(R.id.imageView);
+        // Set the image
+        ivDay1.setImageResource(R.drawable.day1);
+
+        // Get the ImageView obj and assign to ivDay2
         ivDay2 = (ImageView) findViewById(R.id.imageView2);
         // Set the image
         ivDay2.setImageResource(R.drawable.day2);
+
+
     }
 }
